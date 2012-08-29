d1ed8b594b9d906eae16f88cb35999b839daebcd
Oskar Stephens <oskar.stephens@novellus.com>
2012-08-29 13:52:52 -0700

    Initialize rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brewtronic5000::Application.config.secret_token = '1b80065952b7a3f694a7b86212a108ff901dd45f8213bfc947181ea31c5cdd2860256125b7013152415052949631adc6c546801520ca446370b148ccec6ac720'
