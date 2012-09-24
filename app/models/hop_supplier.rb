56fd65a19648d4ce1ba584a4b9600c02a08ec6c1
Oskar Stephens <oskar.stephens@gmail.com>
2012-09-23 21:04:23 -0700

    Rearrange home screen links

--- app/models/hop_supplier.rb
+++ app/models/hop_supplier.rb
@@ -1,5 +1,5 @@
 class HopSupplier < ActiveRecord::Base
   attr_accessible :name
   has_many :hops
-  has_many :hop_inventories:
+  has_many :hop_inventories
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120920044303) do
+ActiveRecord::Schema.define(:version => 20120924034324) do
 
   create_table "brewers", :force => true do |t|
     t.string   "name"
@@ -41,6 +41,19 @@ ActiveRecord::Schema.define(:version => 20120920044303) do
     t.datetime "created_at",        :null => false
     t.datetime "updated_at",        :null => false
   end
+
+  create_table "hop_inventories", :force => true do |t|
+    t.integer  "hop_id"
+    t.integer  "hop_supplier_id"
+    t.integer  "crop_year"
+    t.float    "amount"
+    t.string   "type"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+  end
+
+  add_index "hop_inventories", ["hop_id"], :name => "index_hop_inventories_on_hop_id"
+  add_index "hop_inventories", ["hop_supplier_id"], :name => "index_hop_inventories_on_hop_supplier_id"
 
   create_table "hop_suppliers", :force => true do |t|
     t.text     "name"
