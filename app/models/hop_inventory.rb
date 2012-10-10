4f106f193c7ef57caf1276d918bf653b4aa02046
Bryan Schlief <bschlief@gmail.com>
2012-10-09 18:54:17 -0700

    Utility function for valid year

--- app/models/hop_inventory.rb
+++ app/models/hop_inventory.rb
@@ -1,4 +1,12 @@
 class HopInventory < ActiveRecord::Base
+
+  def is_valid_year?(date_str, start_year=1900, end_year=2099)
+    date_str.grep(/^(\d)+$/) { |date_str|
+      (start_year..end_year).include?(date_str.to_i)
+    }.first
+  end
+
+
   belongs_to :hop
   belongs_to :hop_supplier
   attr_accessible :amount, :crop_year, :type, :hop, :hop_supplier
