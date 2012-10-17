a1241b96b8e25160dc5ee510fe6e021cdbc3545e
Bryan Schlief <bschlief@gmail.com>
2012-10-16 20:48:47 -0700

    Make manufacturer a reference instead of string

--- db/migrate/20121017025628_create_yeasts.rb
+++ db/migrate/20121017025628_create_yeasts.rb
@@ -1,12 +1,15 @@
 class CreateYeasts < ActiveRecord::Migration
   def change
     create_table :yeasts do |t|
+      t.references :manufacturer
       t.string :name
-      t.string :manufacturer
       t.string :yeast_type
       t.float :efficiency
 
       t.timestamps
+
     end
+      
+    add_index :yeasts, :manufacturer_id
   end
 end
