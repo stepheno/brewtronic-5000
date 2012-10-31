d60805524c192eafd0b464a53d0ba460aefc4d6f
Bryan Schlief <bschlief@gmail.com>
2012-10-30 22:16:53 -0700

    Make grain_transactions pretty and navigable

--- app/models/grain_transaction.rb
+++ app/models/grain_transaction.rb
@@ -1,5 +1,8 @@
 class GrainTransaction < ActiveRecord::Base
   belongs_to :grain
   belongs_to :grain_supplier
-  attr_accessible :quantity, :unit_amount
+  attr_accessible :quantity, :unit_amount, :grain_id, :grain_supplier_id
+
+  validates :quantity, :presence => true, :numericality => true
+  validates :unit_amount, :presence => true, :numericality => true
 end
