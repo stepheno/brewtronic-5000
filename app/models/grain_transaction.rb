ca377c776ef14fa92ffc4daa75f02886d0046e4d
Oskar Stephens <oskar.stephens@gmail.com>
2012-11-13 22:50:24 -0800

    Migration for unit_amount to amount in grain trans

--- app/models/grain_transaction.rb
+++ app/models/grain_transaction.rb
@@ -1,10 +1,10 @@
 class GrainTransaction < ActiveRecord::Base
   belongs_to :grain
   belongs_to :grain_supplier
-  attr_accessible :quantity, :unit_amount, :grain_id, :grain_supplier_id
+  attr_accessible :quantity, :amount, :grain_id, :grain_supplier_id
   attr_accessor :unit # Virtual field for units in forms
   attr_accessible :unit # Virtual field for units in forms
 
   validates :quantity, :presence => true, :numericality => true
-  validates :unit_amount, :presence => true, :numericality => true
+  validates :amount, :presence => true, :numericality => true
 end
