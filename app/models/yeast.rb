be6aafffb7db9d0e7043c7651ec3f7ed53cd9a04
Oskar Stephens <oskar.stephens@gmail.com>
2012-12-11 20:55:56 -0800

    Validate presence of yeast manufacturer in yeasts

--- app/models/yeast.rb
+++ app/models/yeast.rb
@@ -2,6 +2,6 @@ class Yeast < ActiveRecord::Base
   attr_accessible :efficiency, :yeast_manufacturer_id, :name, :yeast_type
   belongs_to :yeast_manufacturer
   validates_uniqueness_of :name
-
+  validates :yeast_manufacturer_id, :presence => true
   self.per_page = 10
 end
