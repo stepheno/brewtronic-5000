403f976c76220080b16967231d8226b3a9a490a1
Oskar Stephens <oskar.stephens@gmail.com>
2012-12-11 19:03:55 -0800

    Add grain suppliers to db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,20 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 require 'csv'
 
+grain_suppliers = ['Great Western','Breiss','Rahr','Weyermann','Simpsons']
+grain_suppliers.each { |grain_supplier| GrainSupplier.create(:name => grain_supplier) }
+
 # Insert grains
 CSV.foreach("db/seeds/grains.csv") do |grain|
   newGrain = Grain.new(:name => grain[0], :color => grain[1].to_f, :extract_potential => grain[2].to_f)
   newGrain.save
 end
 
+hop_suppliers = ['Hop Union','BSG']
+hop_suppliers.each { |hop_supplier| HopSupplier.create(:name => hop_supplier) }
+
 # Insert hops 
 CSV.foreach("db/seeds/hops.csv") do |hop|
-  newHop = Hop.new(:name => hop[0], :alpha=> hop[1].to_f, :hop_supplier_id => 1)
+  newHop = Hop.new(:name => hop[0], :alpha=> hop[1].to_f, :hop_supplier_id => HopSupplier.first.id)
   newHop.save
 end
