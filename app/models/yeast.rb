1f09338a9aa3b6659390d57f01c5115b117878d5
Oskar Stephens <oskar.stephens@gmail.com>
2012-12-15 07:18:22 -0800

    Check yeast name uniqueness within manuf scope

--- app/models/yeast.rb
+++ app/models/yeast.rb
@@ -1,7 +1,7 @@
 class Yeast < ActiveRecord::Base
   attr_accessible :efficiency, :yeast_manufacturer_id, :name, :yeast_type
   belongs_to :yeast_manufacturer
-  validates_uniqueness_of :name
+  validates :name, :uniqueness => {:scope => :yeast_manufacturer_id}
   validates :yeast_manufacturer_id, :presence => true
   self.per_page = 10
 end
