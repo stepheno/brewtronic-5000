5c19ab53027fedf8d669f33f6450140d3406bc65
Christopher Quinn <caquinn.86@gmail.com>
2013-01-22 21:21:47 -0800

    fixing Math::E

--- app/models/hop.rb
+++ app/models/hop.rb
@@ -16,7 +16,7 @@ class Hop < ActiveRecord::Base
   self.per_page = 10
 
   def future_alpha
-    self.alpha * (1 / Math::e) * (<HSI> * <tempFactor> * <storageFactor> * <days since last reading>)
+    self.alpha * (1 / Math::E) * (<HSI> * <tempFactor> * <storageFactor> * <days since last reading>)
   end
 
 end
