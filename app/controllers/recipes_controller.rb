8c07c98c58adba04589994924d1e10d47f865e5e
Bryan Schlief <bschlief@gmail.com>
2013-02-26 22:40:46 -0800

    Unit conversion for all the things

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -32,10 +32,25 @@ class RecipesController < ApplicationController
     recipe = params[:recipe]
     recipe[:size] = Units.convert_volume_units(recipe[:size],recipe[:unit])
 
+    recipe[:recipe_hops_attributes].map do |key,hop|
+      hop[:amount] = Units.convert_mass_units(hop[:amount],hop[:unit])
+      recipe[:recipe_hops_attributes][key] = hop 
+    end unless recipe[:recipe_hops_attributes].nil?
+
+    recipe[:recipe_grains_attributes].map do |key,grain|
+      grain[:amount] = Units.convert_mass_units(grain[:amount],grain[:unit])
+      recipe[:recipe_grains_attributes][key] = grain 
+    end unless recipe[:recipe_grains_attributes].nil?
+
+    recipe[:recipe_minerals_attributes].map do |key,mineral|
+      mineral[:amount] = Units.convert_mass_units(mineral[:amount],mineral[:unit])
+      recipe[:recipe_minerals_attributes][key] = mineral 
+    end unless recipe[:recipe_minerals_attributes].nil?
+
     recipe[:recipe_mashes_attributes].map do |key,mash|
       mash[:temperature] = Units.convert_temp_units(mash[:temperature],mash[:unit])
       recipe[:recipe_mashes_attributes][key] = mash 
-    end
+    end unless recipe[:recipe_mashes_attributes].nil?
 
     @recipe = Recipe.new(recipe)
 
