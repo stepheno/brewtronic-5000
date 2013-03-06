c321950d96974a750238e568d61686f47876471d
Oskar Stephens <oskar.stephens@gmail.com>
2013-03-05 18:28:18 -0800

    Fix #27. Validate mash fields

--- app/models/mash_type.rb
+++ app/models/mash_type.rb
@@ -4,5 +4,7 @@ class MashType < ActiveRecord::Base
   has_many :recipe_mashes
   attr_accessible :name
 
+  validates :name, :presence => true
+  validates_uniqueness_of :name
   self.per_page = 10
 end
