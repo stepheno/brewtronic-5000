2ef18ac048e64d51a0cafe5f424b969d33cefc42
Oskar Stephens <oskar.stephens@gmail.com>
2013-03-27 00:57:50 -0700

    Add ajax powers to home page calendar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
+  before_filter :authenticate_user!
   def index
     @batches = Batch.all
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 end
