2877860149cd0bc300db64fb68123e3a0e502949
Oskar Stephens <oskar.stephens@gmail.com>
2013-08-20 23:15:26 -0700

    Summing by grain for used and received

--- app/controllers/reporting_controller.rb
+++ app/controllers/reporting_controller.rb
@@ -6,7 +6,14 @@ class ReportingController < ApplicationController
   def grain
     start_date = params[:start_date]
     end_date = params[:end_date]
-    @grains = GrainTransaction.where(:created_at => start_date..end_date)
+
+    grains_received = GrainTransaction.where(:created_at => start_date..end_date).where("amount > 0")
+    @grain_received_totals = {}
+    grains_received.map {|grain_transaction| @grain_received_totals[grain_transaction.grain] = @grain_received_totals[grain_transaction.grain].to_i + grain_transaction.total_amount }
+
+    grains_used = GrainTransaction.where(:created_at => start_date..end_date).where("amount < 0")
+    @grain_used_totals = {}
+    grains_used.map {|grain_transaction| @grain_used_totals[grain_transaction.grain] = @grain_used_totals[grain_transaction.grain].to_i + grain_transaction.total_amount }
   end
 
   def hop
