b568647c9584e9034c194b13a78025896d715e59
Oskar Stephens <oskar.stephens@gmail.com>
2013-08-27 20:27:10 -0700

    Refactor to generic transactable_used method

--- app/controllers/reporting_controller.rb
+++ app/controllers/reporting_controller.rb
@@ -6,8 +6,8 @@ class ReportingController < ApplicationController
   def grain
     start_date = params[:start_date]
     end_date = params[:end_date]
-    @grain_received_totals = self.grain_received(start_date, end_date)
-    @grain_used_totals = self.grain_used(start_date, end_date)
+    @grain_received_totals = self.transactable_usage(start_date, end_date, GrainTransaction, :grain, "amount > 0")
+    @grain_used_totals = self.transactable_usage(start_date, end_date, GrainTransaction, :grain, "amount < 0")
   end
 
   def hop
@@ -16,18 +16,11 @@ class ReportingController < ApplicationController
   def fermenter
   end
 
-  def grain_received(start_date, end_date, grain = nil)
-    grains_received = GrainTransaction.where(:created_at => start_date..end_date).where("amount > 0")
-    grains_received.reduce({}) do |totals, grain_transaction|
-      totals[grain_transaction.grain] = totals[grain_transaction.grain].to_i + grain_transaction.total_amount
-      totals
-    end
-  end
-
-  def grain_used(start_date, end_date, grain = nil)
-    grains_used = GrainTransaction.where(:created_at => start_date..end_date).where("amount < 0")
-    grains_used.reduce({}) do |totals, grain_transaction|
-      totals[grain_transaction.grain] = totals[grain_transaction.grain].to_i + grain_transaction.total_amount
+  # Takes a date range and a model to operate on, returning the amount used/received
+  def transactable_usage(start_date, end_date, model, foreign_model, conditional, item = nil)
+    amounts = model.where(:created_at => start_date..end_date).where(conditional)
+    amounts.reduce({}) do |totals, transactable|
+      totals[transactable.send foreign_model] = totals[transactable.send foreign_model].to_i + transactable.total_amount
       totals
     end
   end
